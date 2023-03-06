0aecd1d024c3966486849402866d1d4ba888de7b
Bayodele125 <109804362+Bayodele125@users.noreply.github.com>
2023-03-06 16:22:15 +0100

    Update 4-print_float.py

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 number = 3.14159
-print("Float: {:.2f}".format(number))
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+format_float = "{:.2f}".format(number)
+print(format_float)
